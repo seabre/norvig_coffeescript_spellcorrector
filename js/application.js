be9f480e70acec65c424866e82b1c1577e86899b
Sean Brewer <seabre986@gmail.com>
2012-03-03 16:30:07 -0500

    Prettier formatting for application.js

--- js/application.js
+++ js/application.js
@@ -1,24 +1,22 @@
 $(document).ready(function() {
-
-  $(function() {
-    var content = $('#spellinput').val();
-
-    $('#spellinput').keyup(function() { 
-        if ($('#spellinput').val() != content) {
-            content = $('#spellinput').val();
-            if (content == "")
-             { $("#output").html("");}
-            else 
-              { 
-               correction = correct(content);
-               if (correction == content)
-             { $("#output").html("");}
-               else {
-               $("#output").html("Did you mean <em>"+ correct(content) +"</em>?");}
-
-              }
+  return $(function() {
+    var content;
+    content = $("#spellinput").val();
+    return $("#spellinput").keyup(function() {
+      var correction;
+      if ($("#spellinput").val() !== content) {
+        content = $("#spellinput").val();
+        if (content === "") {
+          return $("#output").html("");
+        } else {
+          correction = correct(content);
+          if (correction === content) {
+            return $("#output").html("");
+          } else {
+            return $("#output").html("Did you mean <em>" + correct(content) + "</em>?");
+          }
         }
+      }
     });
-});
-
+  });
 });
